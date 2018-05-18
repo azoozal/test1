24040bcea61036b453052f738644ee26226bc204
Abdulaziz <azooz_280@hotmail.com>
2018-05-18 13:16:50 -0700

    this is the files

--- server/confeg/keys.js
+++ server/confeg/keys.js
@@ -1,5 +1,8 @@
 module.exports = {
 
 
-    mongoURI: 'mongodb://azooz_280:azooz8092542@ds011399.mlab.com:11399/sabic18_aa'
-}
+    mongoURI: 'mongodb://azooz_280:azooz8092542@ds011399.mlab.com:11399/sabic18_aa',
+
+},
+    secretOrKey: 'S3cRe7'
+};
